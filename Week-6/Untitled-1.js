d19885bfef8bfc4804439feb619520163a098a85
BIT Student <classroom@bgit.rs>
2018-05-25 16:26:04 +0200

    pushing excersise-w6

---
+++ Week-6/Untitled-1.js
@@ -0,0 +1,14 @@
+//number one ES6 - practicals
+
+let t = ['hello', 'there', 'ES'];
+
+const output = t.map(x => x[0].toUpperCase().concat(x.slice(1)));
+
+console.log(output);
+
+//number two
+
+const tax = 20;
+
+// let input = 
+
