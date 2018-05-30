ad1bd6bcca1a467bc2f27ef3e0695614079de057
BIT Student <classroom@bgit.rs>
2018-05-30 18:02:57 +0200

    pushing backend mods

---
+++ Week-7/packagemanager/piglet.js
@@ -0,0 +1,15 @@
+var figlet = require('figlet');  
+
+function pig (sth) {
+    var res =  figlet.textSync(smth, {
+            font: 'Ghost'
+        })
+    return res;
+}
+
+module.exports = {
+    pig: pig
+}
+
+
+
