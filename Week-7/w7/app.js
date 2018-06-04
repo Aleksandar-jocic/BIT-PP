af3359bad3e82adb0ccdfcfa91ebe7bf298a35af
BIT Student <classroom@bgit.rs>
2018-06-04 17:48:15 +0200

    pushing reactblog and w7

---
+++ Week-7/w7/app.js
@@ -0,0 +1,14 @@
+const App = () => {
+    return (
+        <div>
+            <h1>De si belce</h1>
+        </div>
+    )
+}
+
+const rootElement = document.getElementById('root');
+
+ReactDOM.render(
+    <App />, 
+    rootElement
+)
